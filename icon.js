f8b82ecb34b8c5f3555d0100422450e0fcecbfe2
eddie.zato <eddie.zato@gmail.com>
2021-12-15 23:50:25 +0700

    Delete icon.js

--- icon.js
+++
@@ -1,8 +0,0 @@
-function updateIcon(c) {
-    if (!c.matches) {
-        document.querySelector('link[rel="icon"]').href = `favw.ico`;    
-    }    
-}
-const m = window.matchMedia("(prefers-color-scheme: dark)");
-m.addEventListener("change", updateIcon);
-updateIcon(m);
