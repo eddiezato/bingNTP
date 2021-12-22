0a100e8c3624ce501ba83e9a65ce1e07fe027369
eddie.zato <eddie.zato@gmail.com>
2021-12-22 13:49:53 +0700

    Add files via upload

--- load.js
+++ load.js
@@ -1,3 +1,4 @@
+document.title = chrome.i18n.getMessage("lnTitle");
 chrome.storage.local.get(['ntpurl'], function(items) {
   if (items.ntpurl.startsWith("https://www.bing.com/")) {
     document.body.style.backgroundImage = "url('" + items.ntpurl + "')";
