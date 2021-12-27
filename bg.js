8a81f67ea5ca88482a9e55ab644d7179b27f0e28
eddie.zato <eddie.zato@gmail.com>
2021-12-27 18:51:17 +0700

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,8 +1,11 @@
 function setbingimage(curdate, message) {
   fetch("https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US")
     .then(res => res.json())
-      .then(out => chrome.storage.local.set({"ntpurl": "https://www.bing.com" + out.images[0].url, "date": curdate}, function() {
-        console.log(message + " - https://www.bing.com" + out.images[0].url);}))
+      .then(out => chrome.storage.local.set({
+        "ntpurl": "https://www.bing.com" + out.images[0].url,
+        "ntptext": out.images[0].copyright.substr(0, out.images[0].copyright.indexOf(" (©")),
+        "date": curdate}, function() {
+          console.log(message + "\nhttps://www.bing.com" + out.images[0].url + "\n" + out.images[0].copyright);}))
     .catch(err => console.log(err));
 }
 
--- load.js
+++ load.js
@@ -1,7 +1,8 @@
 document.title = chrome.i18n.getMessage("lnTitle");
-chrome.storage.local.get(['ntpurl'], function(items) {
+chrome.storage.local.get(['ntpurl', 'ntptext'], function(items) {
   if (items.ntpurl.startsWith("https://www.bing.com/")) {
     document.body.style.backgroundImage = "url('" + items.ntpurl + "')";
+    document.getElementById("p").textContent += items.ntptext;
   }
   else { console.log("bingNTP: No url for background image"); }
 });
