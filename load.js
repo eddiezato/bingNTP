fac5fc043ef8be13e0fd1d3ae1d2adf9d53599f1
eddie.zato <eddie.zato@gmail.com>
2022-01-02 21:46:05 +0700

    Add files via upload

--- load.js
+++ load.js
@@ -2,7 +2,7 @@ document.title = chrome.i18n.getMessage("lnTitle");
 chrome.storage.local.get(['ntpurl', 'ntptext'], function(items) {
   if (items.ntpurl.startsWith("https://www.bing.com/")) {
     document.body.style.backgroundImage = "url('" + items.ntpurl + "')";
-    document.getElementById("p").textContent += items.ntptext;
+    document.getElementById("p").textContent += "\xa0" + items.ntptext + "\xa0";
   }
   else { console.log("bingNTP: No url for background image"); }
 });
